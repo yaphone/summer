3fb3d493d9f43a4d752e1fac4c9050a855ebf598
yaphone <zhouyaphone@163.com>
2017-07-07 23:19:51 +0800

    日常开发

---
+++ src/test/java/cn/zhouyafeng/summer/simple/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.zhouyafeng.summer.simple;
+
+/**
+ * 
+ * @author https://github.com/yaphone
+ * @date 创建时间：2017年7月7日 下午11:19:23
+ * @version 1.0
+ *
+ */
+public class HelloWorld {
+
+}
